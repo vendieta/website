b2291cc57ca0f4a8734fd0fd1a33b03be35a2370
vendieta <vendieta5@gmail.com>
2024-12-02 21:11:44 -0500

    estructura de las tarjetas de producto

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,26 +3,26 @@ import Card  from './components/card';
 function App() {
     return (
     <>
-        <Card
-            ImgSrc="https://i.pinimg.com/originals/b2/fe/89/b2fe89c6539fca539478025bd43687ad.jpg"
-            altText="Sample"
-            description="This is a great product."
-            price={29.99}/>
-        <Card 
-            ImageSrc="https://via.placeholder.com/150"
-            altText="Sample Product"
-            description="This is a great product."
-            price={29.99}/>
-        <Card 
-            ImageSrc="https://via.placeholder.com/150"
-            altText="Sample Product"
-            description="This is a great product."
-            price={29.99}
-        />
+        <section className='card-container'>
+            <Card
+                ImgSrc="https://i.pinimg.com/originals/b2/fe/89/b2fe89c6539fca539478025bd43687ad.jpg"
+                altText="Sample"
+                description="This is a great product. about product is more beutiful because yes"
+                price={29.99}/>
+            <Card 
+                ImageSrc="https://via.placeholder.com/150"
+                altText="Sample Product"
+                description="This is a great product."
+                price={29.99}/>
+            <Card 
+                ImageSrc="https://via.placeholder.com/150"
+                altText="Sample Product"
+                description="This is a great product. about product is more beutiful because yes"
+                price={29.99}
+            />
+        </section>
     </>
-)
-
+    )
 
 }
-
 export default App;
--- front-end/src/components/card.jsx
+++ front-end/src/components/card.jsx
@@ -5,11 +5,15 @@ function Card({ ImgSrc , altText , description , price }){
 
     return (
     <div className="card-product">
-        <img className="img-product" src={ImgSrc} alt={altText} />
+        <div className="img-product-container" >
+            <a href="">
+                <img className="img-product" src={ImgSrc} alt={altText} />
+            </a>
+        </div>
         <section className="info-product">
             <p className="description-product">{description}</p>
             <span className="price-product">${price}</span>
-            <button className='buy-product' >Comprar</button>
+            <button className='buy-product' g>Comprar</button>
         </section>
     </div>
     )
